0fcbac7f760959505ca9643a8779abe31092b4f7
oBzB <oBzB_2@172.21.11.245>
2012-11-12 11:56:20 +0100

    vytvořeny grafy

--- SensorLogger/src/com/obzb/sensorlogger/MainActivity.java
+++ SensorLogger/src/com/obzb/sensorlogger/MainActivity.java
@@ -2,7 +2,10 @@ package com.obzb.sensorlogger;
 
 import java.util.ArrayList;
 
+
 import com.obzb.sensorlogger.classes.ISensor;
+import com.obzb.sensorlogger.classes.SGraph;
+import com.obzb.sensorlogger.classes.SLogger;
 import com.obzb.sensorlogger.classes.Sensors;
 
 import android.hardware.Sensor;
@@ -10,23 +13,34 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.SparseIntArray;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends Activity{
 	public static Context CONTEXT;
 	//GUI prvky
-	TextView txtLog;
+	TextView txtInfo;
+	TextView txtData;
 	Spinner spSensors;
+	LinearLayout lGraph;
 	ArrayList<String> lSensors = new ArrayList<String>();
 	SparseIntArray priSenzory = new SparseIntArray(); //pøiøazuje id typu senzoru k jeho poøadí ve spinneru, sparseIntArray má být výkonnìjší než HashMap
+	ImageButton bPlay;
+	ImageButton bPause;
+	ImageButton bStop;
+	
+	private int lState = 0; //stav logování, 0 vypnuto, 1 pauza, 2 bìží
 	
 	// tøídy pro senzory
 	public static SensorManager SMANAGER;
@@ -34,18 +48,30 @@ public class MainActivity extends Activity{
 	private Sensors sensors;
 	private ISensor isensor;
 	private SensorEventListener slistener;
+	private SLogger logger;
+	private SGraph graph;
+	private int sParams; //poèet parametrù od senzoru
+	private int i=0; //poèítadlo pro graf
+	
+	//private final Handler mHandler = new Handler(); //zajištuje refresh grafu - nezajištuje
+	//private Runnable mTimer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lGraph = (LinearLayout) findViewById(R.id.lGraph);
         CONTEXT = this;
         SMANAGER = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensors = new Sensors();
         vytvorSlistener();
         //GUI inicializace
-        txtLog = (TextView) findViewById(R.id.txtLog);
+        txtInfo = (TextView) findViewById(R.id.txtInfo);
+        txtData = (TextView) findViewById(R.id.txtData);
         spSensors = (Spinner)findViewById(R.id.spSensors);
+        bPlay = (ImageButton) findViewById(R.id.bPlay);
+        bPause = (ImageButton) findViewById(R.id.bPause);
+        bStop = (ImageButton) findViewById(R.id.bStop);
         
         naplnSpinner();
         ArrayAdapter<String> arSensors = new ArrayAdapter<String>(this, R.layout.row, lSensors);
@@ -59,34 +85,101 @@ public class MainActivity extends Activity{
 				SMANAGER.unregisterListener(slistener);
 				isensor = sensors.getISensor(priSenzory.get(pozice));
 				sensor = isensor.getSensor();
-				String nazev = sensor.getName();
-				txtLog.setText(nazev);
+				txtInfo.append(getString(R.string.name)+sensor.getName()+"\n");
+				txtInfo.append(getString(R.string.vendor)+sensor.getVendor()+"\n");
+				txtInfo.append(getString(R.string.power)+sensor.getPower()+" mA\n");
+				txtInfo.append(getString(R.string.maxrange)+sensor.getMaximumRange()+"\n");
+				txtInfo.append(getString(R.string.resolution)+sensor.getResolution()+"\n");
+				txtInfo.append(getString(R.string.mindelay)+sensor.getMinDelay()+" ms\n");
+				txtInfo.append(isensor.getNote());
+				
+				sParams = isensor.getValuesNames().length;
+				logger = new SLogger();
+				graph = new SGraph();
+			    lGraph.removeAllViews();  //odstraní pøípadný pøedešlý graf
+			    lGraph.addView(graph.kresliGraf(sParams, isensor.getValuesNames()));  
 				SMANAGER.registerListener(slistener, sensor, SensorManager.SENSOR_DELAY_UI);
 				
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
-				txtLog.setText("Nic nevybráno");
+				txtInfo.setText("Nic nevybráno");
 			}
 		});
+        
+        bPlay.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				lState = 2;
+				
+			}
+		});
+        
+        bPause.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				lState = 1;
+				
+			}
+		});
+        bStop.setOnClickListener(new View.OnClickListener() {
+	
+        	@Override
+        	public void onClick(View v) {
+        		lState = 0;
+        		i=0;
+        		graph.reset();
+        		lGraph.invalidate();
+		
+        	}
+        });
+        
         
     }
     
     private void vytvorSlistener() {
 		slistener = new SensorEventListener() {
-			
+			float x;
+			float y;
+			float z;
 			@Override
 			public void onSensorChanged(SensorEvent event) {
-				txtLog.setText(isensor.getValuesNames()[0][0]+String.valueOf(event.values[0])+isensor.getValuesNames()[0][1]+"\n"
-						+isensor.getValuesNames()[1][0]+String.valueOf(event.values[1])+isensor.getValuesNames()[1][1]+"\n"
-						+isensor.getValuesNames()[2][0]+String.valueOf(event.values[2])+isensor.getValuesNames()[2][1]);
-				
+				String[][] sValues = isensor.getValuesNames();
+				if (lState == 2){
+					txtData.setText("");
+					for (int j=0; j<sParams; j++){//cyklus kdy to bude postupnì apendovat øádky dle poètu parametrù
+						txtData.append(sValues[j][0]+" "+String.valueOf(event.values[j])+" "+sValues[j][1]+"\n");
+					};
+					
+					//logger.addValues(event.values[0]);
+					
+					x = event.values[0];
+					y = event.values[1];
+					z = event.values[2];
+					/*mTimer = new Runnable() {    //dìsnì zasekává aplikaci
+						@Override
+						public void run() {
+							if ( (i % 400) == 0) graph.reset();
+							graph.pridejHodnotu(i, x, y, z);
+							mHandler.postDelayed(this, 2000);
+							lGraph.invalidate();
+						}
+					};
+					mHandler.postDelayed(mTimer, 2000);
+					i++;*/
+					graph.pridejHodnotu(i, x, y, z);
+					
+					lGraph.invalidate();
+					i++;
+				}
 			}
 			
 			@Override
 			public void onAccuracyChanged(Sensor sensor, int accuracy) {
-				// TODO Auto-generated method stub
+				
 				
 			}
 		};
@@ -141,6 +234,12 @@ public class MainActivity extends Activity{
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    
     }
     
 }
